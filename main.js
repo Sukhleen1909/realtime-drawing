d87a5021b221c94108077d8dbd00aca2dd35c9c8
Sukhleen1909 <86345274+Sukhleen1909@users.noreply.github.com>
2022-06-23 15:49:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(550,500);
+canvas=createCanvas(550,550);
+canvas.position(560,200);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded() {
+    console.log('Posenet is intialised');
+}
+function draw() {
+    background('#87CEEB');
+    document.getElementById("square_sides").innerHTML="Width and Height of a square will be = "+difference+"px";
+    fill('#FFA500');
+    stroke('#FFA500');
+    square(noseX,noseY,difference);
+}
+function gotPoses(results) {
+    if(results.length>0) {
+        console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log("noseX= "+noseX+ "noseY= "+noseY);
+leftwristX=results[0].pose.leftWrist.x;
+rightwristX=results[0].pose.rightWrist.x;
+difference=floor(leftwristX-rightwristX);
+console.log("leftwristX= "+leftwristX+"rightwristX= "+rightwristX+"difference= "+difference);
+    }
+}
